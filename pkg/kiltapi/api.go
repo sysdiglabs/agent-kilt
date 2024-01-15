83a41eb03ba72d8def890215924b10742f8df86a
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2024-01-15 11:42:42 +0100

    cleanup: remove dead code

--- pkg/kiltapi/api.go
+++ pkg/kiltapi/api.go
@@ -5,11 +5,6 @@ import (
 	"github.com/sysdiglabs/agent-kilt/pkg/kilt"
 )
 
-func NewKiltFromHocon(definition string) *kilt.Kilt {
-	impl := hocon.NewKiltHocon(definition)
-	return kilt.NewKilt(impl)
-}
-
 func NewKiltFromHoconWithConfig(definition string, config string) *kilt.Kilt {
 	impl := hocon.NewKiltHoconWithConfig(definition, config)
 	return kilt.NewKilt(impl)
