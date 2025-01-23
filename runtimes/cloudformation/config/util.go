273a686899a63cd311c43b8142d05269b180dfed
Paul Crooks <paul.crooks@sysdig.com>
2025-01-23 11:32:06 +0000

    chore(deps): Stop using deprecated ioutil

--- runtimes/cloudformation/config/util.go
+++ runtimes/cloudformation/config/util.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"compress/gzip"
 	"fmt"
-	"io/ioutil"
+	"io"
 )
 
 func decompressBytes(data []byte) []byte {
@@ -13,7 +13,7 @@ func decompressBytes(data []byte) []byte {
 		panic(fmt.Errorf("error constructing decompressor: %w", err))
 	}
 
-	decompressedData, err := ioutil.ReadAll(reader)
+	decompressedData, err := io.ReadAll(reader)
 	if err != nil {
 		panic(fmt.Errorf("error reading gzipped stream: %w", err))
 	}
